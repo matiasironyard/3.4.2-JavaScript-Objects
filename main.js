1e62f3b87f94d60881d617690300a4c7a461ae98
Matias Mariani <matiasironyard@gmail.com>
2016-10-02 02:35:18 -0400

    working on js

--- main.js
+++ main.js
@@ -4,24 +4,57 @@
 
 //------filter words above 3 characters from array--------//
 var wordsAboveThree = commonWords.filter(function(word){
-  var randWordArray = [];
-  if(word.length > 3){
-  return randWordArray.push(word);
+  // var randWordArray = [ ];
+  if(word.length >= 3){
+  // return randWordArray.push(word);
+  return word;
 }
 });
 
 //------random select a word from above---------//
-var  challengeWord = wordsAboveThree[Math.floor(Math.random() * wordsAboveThree.length)];
+function randomWordGenerator (){
+  var  randSelector= Math.floor(Math.random() * wordsAboveThree.length);
+  return wordsAboveThree[randSelector];
+}
+
+var generatedWord = randomWordGenerator();
+
+
+var wordBox = document.querySelector(".spangenerate");
+
+//--------event listener for guess button--------//
+var button = document.getElementById("button");
+button.addEventListener("click", guessFetcher);
+console.log(button);
+
+//-----------guessAnalyzer compares guess input against random word---------///
+function guessFetcher(){
+  var myGuess = document.getElementById("my-input");
+  var myLetter = myGuess.value;
+  myGuess = "";
+
+  console.log("this is my guess" + myLetter);
+}
+
+function compare () {
+  if (wordsAboveThree[i] === guessFetcher()){
+
+  }
+}
+
+console.log(guessFetcher());
+console.log(generatedWord);
+console.log(wordBox);
 console.log(wordsAboveThree);
-console.log("challenge word:" + challengeWord);
+console.log("challenge word:" + generatedWord);
 
 //------breakdown word into letters--------//
 
-for(var i = 0; i < challengeWord.length; i ++){
-  var wordLetters = challengeWord[i];
-  // document.getElementById("challenge-word").innerHTML= wordLetters;
-  console.log(wordLetters);
-}
+// for(var i = 0; i < challengeWord.length; i ++){
+//   var wordLetters = challengeWord[i];
+//   // document.getElementById("challenge-word").innerHTML= wordLetters;
+//   console.log(wordLetters);
+// }
 
 // var wordLength = new Array(challengeWord.length);
 // console.log(wordLength);
